5ef3ee469570127f598b7b7422c8fa4197fa95f8
ZethGecko <nelsonluque098@gmail.com>
2025-04-20 22:35:41 -0500

    inicio de un nuevo proyecto

---
+++ ms-microservicio1/src/main/java/com/example/msmicroservicio1/MsMicroservicio1Application.java
@@ -0,0 +1,13 @@
+package com.example.msmicroservicio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMicroservicio1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsMicroservicio1Application.class, args);
+    }
+
+}
---
+++ ms-microservicio2/src/main/java/com/example/msmicroservicio2/MsMicroservicio2Application.java
@@ -0,0 +1,13 @@
+package com.example.msmicroservicio2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMicroservicio2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsMicroservicio2Application.class, args);
+    }
+
+}
