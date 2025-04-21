07b2a98293524376ad6db7210ec365613a8ac199
ZethGecko <nelsonluque098@gmail.com>
2025-04-21 12:35:40 -0500

    examen

--- ms-matricula-service/src/main/java/com/example/msmatriculaservice/entity/Matricula.java
+++ ms-matricula-service/src/main/java/com/example/msmatriculaservice/entity/Matricula.java
@@ -1,8 +1,21 @@
 package com.example.msmatriculaservice.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class Matricula {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String estado;
+    private Integer cursoId; // Foreign key simulada del curso
+    private Integer alumnoId; // Foreign key simulada del alumno
 
+    private Integer ciclo; // Ciclo de la matrícula
+    private LocalDateTime fechaMatricula; // Fecha de matrícula
 }
