1617364623c90475de63b4a5b7d991c552794121
ZethGecko <nelsonluque098@gmail.com>
2025-05-15 08:57:57 -0500

    condiguracion auth

--- ms-auth/src/main/java/com/example/msauth/repository/AuthUserRepository.java
+++ ms-auth/src/main/java/com/example/msauth/repository/AuthUserRepository.java
@@ -7,8 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-
-public interface AuthUserRepository extends JpaRepository<AuthUser,Long> {
-        Optional<AuthUser> findByUsername(String userName); // Ajusta según el nombre correcto
-    }
+public interface AuthUserRepository extends JpaRepository<AuthUser, Long> {
+    Optional<AuthUser> findByUserName(String userName); // Coincide con la entidad
+}
 
